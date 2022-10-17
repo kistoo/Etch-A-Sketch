a492bc8d9810da8a955fc6b1b445d7292968718f
kistoo <tomio.nakamura15@gmail.com>
2022-10-17 12:26:53 -0500

    Fix error in paint
    Now paint will run on click

--- script.js
+++ script.js
@@ -6,25 +6,27 @@ let paintStatus = false;
 generateSketch();
 input.onchange = () => generateSketch();
 
-
-
 function generateSketch(){
     canvas.textContent="";
     //create a matrix
     for (let i = 0 ; i<sketchSide.textContent;i++){
         for (let j = 0; j<sketchSide.textContent;j++){
         let div = document.createElement("div");
-        //toggle paint status
+        div.setAttribute("style",
+        `width:${800/sketchSide.textContent}px;
+        height:${800/sketchSide.textContent}px`);
+        //toggle paint status 
         div.addEventListener("mousedown",()=>paintStatus=true);
         div.addEventListener("mouseup",()=>paintStatus=false);
-        div.setAttribute("style",`width:${800/sketchSide.textContent}px; height:${800/sketchSide.textContent}px`);
+        //paints
         div.addEventListener("mouseover",(e)=>paint(e));
+        div.addEventListener("click",(e)=>paint(e));
         canvas.appendChild(div);
         }
     }
 }
 
 function paint(e){
-    if (paintStatus===true){
+    if (paintStatus===true || e.type==="click"){
         e.target.style.backgroundColor="black";}
 }
