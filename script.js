d1d18b8660b1b1497fb63373e3da29e5d90d39cc
kistoo <tomio.nakamura15@gmail.com>
2022-10-17 11:58:58 -0500

    Add paint function

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const sketchSide = document.querySelector("output");
 const input = document.getElementById("slider");
 const canvas = document.getElementById("canvas");
+let paintStatus = false;
 
+generateSketch();
 input.onchange = () => generateSketch();
+
+
 
 function generateSketch(){
     canvas.textContent="";
@@ -10,8 +14,17 @@ function generateSketch(){
     for (let i = 0 ; i<sketchSide.textContent;i++){
         for (let j = 0; j<sketchSide.textContent;j++){
         let div = document.createElement("div");
-        div.setAttribute("style",`width:${800/sketchSide.textContent}px; height:${800/sketchSide.textContent}px`)
+        //toggle paint status
+        div.addEventListener("mousedown",()=>paintStatus=true);
+        div.addEventListener("mouseup",()=>paintStatus=false);
+        div.setAttribute("style",`width:${800/sketchSide.textContent}px; height:${800/sketchSide.textContent}px`);
+        div.addEventListener("mouseover",(e)=>paint(e));
         canvas.appendChild(div);
         }
     }
+}
+
+function paint(e){
+    if (paintStatus===true){
+        e.target.style.backgroundColor="black";}
 }
